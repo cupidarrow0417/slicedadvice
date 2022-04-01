5a4c3791cc5a96234d864cea08e42e572f5110ec
Adrian Jake <cupidarrow0417@gmail.com>
2022-04-01 17:53:09 -0500

    majority of files for previous commit message <-

--- components/expertisePost/ExpertisePostDetails.tsx
+++ components/expertisePost/ExpertisePostDetails.tsx
@@ -6,6 +6,7 @@ import { clearErrors } from "../../redux/actions/expertisePostActions"
 
 import Breadcrumbs from "../Breadcrumbs"
 import Image from "next/image"
+import RatingsWidget from "../RatingsWidget"
 
 
 
@@ -26,22 +27,34 @@ const ExpertisePostDetails = () => {
       <Head>
         <title>{expertisePost.title} | SlicedAdvice</title>
       </Head>
-      <div className="flex flex-col gap-5 py-6 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div className="flex flex-col items-start gap-5 py-6 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <Breadcrumbs />
         <h1 className="text-2xl font-semibold">
           {expertisePost["title"]}
         </h1>
-        <div className="expertisePostDetailImageWrapper">
-          {
-            expertisePost["images"][0] &&
-            <Image
-              src={expertisePost["images"][0]["url"]}
-              layout="responsive"
-              width={1}
-              height={1}
-              alt="Picture for expertise posting"
-            />
-          }
+        <RatingsWidget expertisePost={expertisePost} />
+
+        <div className="flex flex-col md:flex-row justify-start items-center w-full gap-7 mt-3 ">
+          <div className="expertisePostDetailImageWrapper w-4/5 self-center">
+            {
+              expertisePost["images"][0] &&
+              <Image
+                src={expertisePost["images"][0]["url"]}
+                layout="responsive"
+                width={1.5}
+                height={1}
+                alt="Picture for expertise posting"
+              />
+            }
+          </div>
+          <div className="flex flex-col w-full md:max-w-lg">
+            <h1 className="text-xl font-medium">
+              Description
+            </h1>
+            <p className="font-light opacity-60">
+              {expertisePost["description"]}
+            </p>
+          </div>
         </div>
       </div>
     </>
