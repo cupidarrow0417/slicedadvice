c9a410f464aedbbc2e5281ee266df3ed5a872ed3
Adrian Jake <cupidarrow0417@gmail.com>
2022-08-08 19:19:25 +0300

    Fixed a bug where an empty email was being sent after the original email

--- controllers/webhookControllers.ts
+++ controllers/webhookControllers.ts
@@ -51,7 +51,7 @@ async function sendRefundEmail(details: any) {
     });
 
     console.log("Message sent: %s", info.messageId);
-    await transporter.sendMail(info);
+    // await transporter.sendMail(info);
 
     // --- ETHEREAL TESTING STUFF ---
     // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
